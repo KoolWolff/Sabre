77c7440e9338761c089cc6daa09161d8e9695951
Crauzer <Crauzer@DESKTOP-CI4BL1F>
2016-12-11 15:31:13 +0100

    Added the EasterEgg class which will be used for easter eggs

---
+++ Sabre/Code/Main/EasterEgg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabre
+{
+    class EasterEgg
+    {
+        public EasterEgg(EasterEggType type, MainWindow mw)
+        {
+
+        }
+        public enum EasterEggType
+        {
+            Doggo,
+            Crauzer,
+            Brokoli,
+            Chewy,
+            Yurixy,
+            Leischii
+        }
+    }
+}
